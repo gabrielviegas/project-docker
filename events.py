2996040ed618b5ae7913cad63f52471acc485a0c
Gabriel Viegas <gabrielviegasneto@gmail.com>
2025-02-18 16:52:36 -0300

    Adjust and print on screen

--- events.py
+++ events.py
@@ -3,5 +3,8 @@ import docker
 client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
 
 for event in client.events(decode=True, filters={"event": "die"}):
-    print(event)
+    container_id = event["id]
+    container_name = event["Actor"]["Atributes"]["name"]
+    epoch_time = event["time"]
+    print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
 
