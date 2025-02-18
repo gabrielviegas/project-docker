76f4b1500ac26b3e3bc1f531e1038da8b89bd732
Gabriel Viegas <gabrielviegasneto@gmail.com>
2025-02-18 17:02:09 -0300

    Adjusting the date

--- events.py
+++ events.py
@@ -1,10 +1,11 @@
 import docker
-
+import datetime
 client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
 
 for event in client.events(decode=True, filters={"event": "die"}):
     container_id = event["id"]
     container_name = event["Actor"]["Atributes"]["name"]
     epoch_time = event["time"]
-    print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
+    date_time = datetime.datetime.fromtimestamp(epoch_time)
+    print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time))
 
