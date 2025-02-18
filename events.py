9d44a8ff758b450f49fece6f166e0be99bf8b334
Gabriel Viegas <gabrielviegasneto@gmail.com>
2025-02-18 16:54:11 -0300

    inserting quotes in id

--- events.py
+++ events.py
@@ -3,7 +3,7 @@ import docker
 client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
 
 for event in client.events(decode=True, filters={"event": "die"}):
-    container_id = event["id]
+    container_id = event["id"]
     container_name = event["Actor"]["Atributes"]["name"]
     epoch_time = event["time"]
     print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
